e24ee2dcb49891a80ee2f6154097a2d71154d162
qingqingqingli <kadlqbetty@gmail.com>
2021-02-25 11:35:54 +0100

    add catch2 test files

---
+++ cmake-build-debug/CMakeFiles/UNIT_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UNIT_TEST.dir/unit_test/catch2/catch.cpp.o"
+  "CMakeFiles/UNIT_TEST.dir/unit_test/factorial.cpp.o"
+  "UNIT_TEST"
+  "UNIT_TEST.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UNIT_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ unit_test/factorial.cpp
@@ -0,0 +1,20 @@
+//
+// Created by qli on 24/02/2021.
+//
+
+// Let Catch provide main():
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+int Factorial( int number ) {
+	return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
+// return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
+	REQUIRE(Factorial(0) == 1);
+}
