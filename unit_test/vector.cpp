e55abf8b9d04543c32a735fafb069ee49394c246
qingqingqingli <kadlqbetty@gmail.com>
2021-02-26 14:09:04 +0100

    update vector test cases

--- unit_test/vector.cpp
+++ unit_test/vector.cpp
@@ -9,53 +9,15 @@ TEST_CASE("vector empty constructor", "[vector]") {
 	ft::vector<char> custom1(123);
 	std::vector<int> original2(0);
 	ft::vector<int> custom2(0);
+	std::vector<float> original3(45);
+	ft::vector<float> custom3(45);
 
 	SECTION("test initialised value") {
 		REQUIRE(custom1.size() == original1.size());
 		REQUIRE(custom1.capacity() == original1.capacity());
 		REQUIRE(custom2.size() == original2.size());
 		REQUIRE(custom2.capacity() == original2.capacity());
+		REQUIRE(custom3.size() == original3.size());
+		REQUIRE(custom3.capacity() == original3.capacity());
 	}
 }
-
-
-//TEST_CASE( "test vector constructors", "[vector]" ) {
-//	// initialization block
-//	std::vector<int> first;
-//	std::vector<int> second (4,100);
-//	std::vector<int> third (second.begin(),second.end());
-//	std::vector<int> fourth (third);
-//
-//	// check initial size & capacity
-//	REQUIRE(first.size() == 0);
-//	REQUIRE(first.capacity() == 0);
-//	REQUIRE(second.size() == 4);
-//	REQUIRE(second.capacity() == 4);
-//	REQUIRE(third.size() == 4);
-//	REQUIRE(third.capacity() == 4);
-//	REQUIRE(fourth.size() == 4);
-//	REQUIRE(fourth.capacity() == 4);
-//
-//	SECTION("resize vector") {
-//		// increase to 250 -> size and capacity change to 250
-//		second.resize(250);
-//		REQUIRE(second.size() == 250);
-//		REQUIRE(second.capacity() == 250);
-//		// resize to 0 -> size changes to 0 and capacity remains
-//		second.resize(0);
-//		REQUIRE(second.size() == 0);
-//		REQUIRE(second.capacity() == 250);
-//		// increase to 1024 -> size && capacity == 1024
-//		second.resize(1024);
-//		REQUIRE(second.size() == 1024);
-//		REQUIRE(second.capacity() == 1024);
-//		// pushback an element -> size += 1 & capacity *= 2
-//		second.push_back(80);
-//		REQUIRE(second.size() == 1025);
-//		REQUIRE(second.capacity() == 2048);
-//		// pushback an element -> size == 20 & capacity remains
-//		second.resize(20);
-//		REQUIRE(second.size() == 20);
-//		REQUIRE(second.capacity() == 2048);
-//	}
-//}
