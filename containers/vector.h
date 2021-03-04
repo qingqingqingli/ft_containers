36a9f26e8ba86ddbd4b65f460385033948ef0694
qingqingqingli <kadlqbetty@gmail.com>
2021-03-04 14:08:59 +0100

    update structure of vector

--- containers/vector.h
+++ containers/vector.h
@@ -38,12 +38,12 @@ private:
 
 public:
 
-//-> coplien form
+//---------------------------------------Coplien form-----------------------------------
 
-	//** 1. default constructor: empty vector
+	//** [coplien form] default constructor: empty vector
 	explicit vector (const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _alloc(alloc), _array(NULL) {}
 
-	//** 2. default constructor: vector with size and val
+	//** [coplien form] default constructor: vector with size and val
 	explicit vector (size_type n, const value_type& val = value_type(),const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _alloc(alloc)
 	{
 		this->_array = new value_type [n];
@@ -55,18 +55,17 @@ public:
 		}
 	}
 
-	//** 3. default constructor: vector with InputIterator
-	// for later to implement the iterator
+	//** [coplien form] default constructor: vector with InputIterator
 //	template <class InputIterator>	vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {
 //
 //	}
 
-	//** 4. copy constructor
+	//** [coplien form] copy constructor
 	vector (const vector& x){
 		*this = x;
 	}
 
-	//** 5. assignation operator ->update later
+	//** [coplien form] assignation operator ->update later
 	vector& operator= (const vector& x) {
 		if (this != &x)
 		{
@@ -85,20 +84,68 @@ public:
 		return *this;
 	}
 
-	//** 6. default destructor
+	//** [coplien form] default destructor
 	~vector() {
 		delete [] this->_array;
 	}
 
-//-> member functions
+//---------------------------------------Iterators----------------------------------
+	//** [iterator] begin (returns an iterator pointing to the first element in the vector)
+	iterator begin();
+	const_iterator begin() const;
 
-	//** 7. assign (assign new contents to the vector)
+	//** [iterator] end (return iterator to end)
+	iterator end();
+	const_iterator end() const;
 
-	template <class InputIterator> void assign (InputIterator first, InputIterator last);
+	//** [iterator] rbegin (return a reverse iterator pointing to the last element in the vector)
+	reverse_iterator rbegin();
+	const_reverse_iterator rbegin() const;
 
-	void assign (size_type n, const value_type& val);
+	//** [iterator] rend (return a reverse iterator pointing to the theoretical element preceding the first element in the vector)
+	reverse_iterator rend();
+	const_reverse_iterator rend() const;
 
-	//** 8. at (returns a reference to the element at position n in the vector)
+//---------------------------------------Capacity-----------------------------------
+
+	//** [capacity] size (return the number of elements in the vector)
+	size_type size() const {
+		return this->_size;
+	}
+
+	//** [capacity] max_size (return the max number of elements)
+	// to understand the difference between capacity and max_size
+	size_type max_size() const;
+
+	//** [capacity] resize (resize the container so that it contains n elements)
+	void resize (size_type n, value_type val = value_type());
+
+	//** [capacity] capacity (return the size of the storage space)
+	size_type capacity() const {
+		return this->_capacity;
+	}
+
+	//** [capacity] empty (test whether the vector is empty)
+	bool empty() const;
+
+	//** [capacity] reverse (request that the vector capacity be at least enough to contain n elements)
+	void reserve (size_type n);
+
+
+//---------------------------------------Element access-----------------------------
+
+	//** [element access] operator[] (return a reference to the element at position n)
+	reference operator[] (size_type n) {
+		T& elem_ref = *(this->_array + n);
+		return elem_ref;
+	}
+
+	const_reference operator[] (size_type n) const {
+		const T& elem_ref = *(this->_array + n);
+		return elem_ref;
+	}
+
+	//** [element access] at (returns a reference to the element at position n in the vector)
 	// the first element has a position of 0
 
 	reference at (size_type n) {
@@ -131,7 +178,11 @@ public:
 		}
 	}
 
-	//** 9. back (returns a reference to the last element in the vector)
+	//** [element access] front (return a reference to the first element)
+	reference front();
+	const_reference front() const;
+
+	//** [element access] back (returns a reference to the last element in the vector)
 	reference back() {
 		reference back_ref = this->_array[this->size() - 1];
 		return back_ref;
@@ -142,60 +193,17 @@ public:
 		return const_back_ref;
 	}
 
-	//** 10. begin (returns an iterator pointing to the first element in the vector)
-	iterator begin();
-	const_iterator begin() const;
 
-	//** 11. capacity (return the size of the storage space)
-	size_type capacity() const {
-		return this->_capacity;
-	}
 
-	//** 12. clear (remove all elements from the vector)
-	void clear();
+//---------------------------------------Modifier-----------------------------------
 
-	//** 13. empty (test whether the vector is empty)
-	bool empty() const;
+	//** [element access] assign (assign new contents to the vector)
 
-	//** 14. end (return iterator to end)
-	iterator end();
-	const_iterator end() const;
+	template <class InputIterator> void assign (InputIterator first, InputIterator last);
 
-	//** 15. erase (erase element(s) from the vector)
-	iterator erase (iterator position);
-	iterator erase (iterator first, iterator last);
+	void assign (size_type n, const value_type& val);
 
-	//** 16. front (return a reference to the first element)
-	reference front();
-	const_reference front() const;
-
-	//** 17. insert (insert new elements to the vector)
-	iterator insert (iterator position, const value_type& val);
-
-	void insert (iterator position, size_type n, const value_type& val);
-
-	template <class InputIterator> void insert (iterator position, InputIterator first, InputIterator last);
-
-	//** 18. max_size (return the max number of elements)
-	// to understand the difference between capacity and max_size
-	size_type max_size() const;
-
-	//** 19. operator[] (return a reference to the element at position n)
-	// does not check against bounds
-	reference operator[] (size_type n) {
-		T& elem_ref = *(this->_array + n);
-		return elem_ref;
-	}
-
-	const_reference operator[] (size_type n) const {
-		const T& elem_ref = *(this->_array + n);
-		return elem_ref;
-	}
-
-	//** 20. pop_back (removes the last element in the vector)
-	void pop_back();
-
-	//** 21. push_back (add a new element at the end of the vector)
+	//** [element access] push_back (add a new element at the end of the vector)
 	void push_back (const value_type& val) {
 		if (this->size() + 1 > this->capacity())
 		{
@@ -222,27 +230,25 @@ public:
 		this->_size += 1;
 	}
 
-	//** 22. rbegin (return a reverse iterator pointing to the last element in the vector)
-	reverse_iterator rbegin();
-	const_reverse_iterator rbegin() const;
+	//** [element access] pop_back (removes the last element in the vector)
+	void pop_back();
 
-	//** 23. rend (return a reverse iterator pointing to the theoretical element preceding the first element in the vector)
-	reverse_iterator rend();
-	const_reverse_iterator rend() const;
+	//** [element access] insert (insert new elements to the vector)
+	iterator insert (iterator position, const value_type& val);
 
-	//** 24. reverse (request that the vector capacity be at least enough to contain n elements)
-	void reserve (size_type n);
+	void insert (iterator position, size_type n, const value_type& val);
 
-	//** 25. resize (resize the container so that it contains n elements)
-	void resize (size_type n, value_type val = value_type());
+	template <class InputIterator> void insert (iterator position, InputIterator first, InputIterator last);
 
-	//** 26. size (return the number of elements in the vector)
-	size_type size() const {
-		return this->_size;
-	}
+	//** [element access] erase (erase element(s) from the vector)
+	iterator erase (iterator position);
+	iterator erase (iterator first, iterator last);
 
-	//** 27. swap (exchange the content of the container by the content of another container)
+	//** [element access] swap (exchange the content of the container by the content of another container)
 	void swap (vector& x);
+
+	//** [element access] clear (remove all elements from the vector)
+	void clear();
 
 };
 }
