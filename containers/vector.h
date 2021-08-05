c8bf38375e2c5f0d268bbda9f358a4901914fe4e
qingqingqingli <kadlqbetty@gmail.com>
2021-08-05 16:27:45 +0200

    Remove this->

--- containers/vector.h
+++ containers/vector.h
@@ -37,36 +37,36 @@ private:
 	void reallocation(size_type n)
 	{
 		value_type *new_array = new value_type [n];
-		for (size_type i = 0; i < this->size(); i++)
-			new_array[i] = this->_array[i];
-		delete [] this->_array;
-		this->_array = new_array;
-		this->_capacity = n;
+		for (size_type i = 0; i < _size; i++)
+			new_array[i] = _array[i];
+		delete [] _array;
+		_array = new_array;
+		_capacity = n;
 	}
 
 	void reallocation_new_value(size_type n, value_type val)
 	{
 		value_type *new_array = new value_type [n];
-		this->_capacity = n;
-		for (size_type i = 0; i < this->size(); i++)
+		_capacity = n;
+		for (size_type i = 0; i < _size; i++)
 			new_array[i] = val;
-		delete [] this->_array;
-		this->_array = new_array;
+		delete [] _array;
+		_array = new_array;
 	}
 
 	void copy_vector_value(size_type n, value_type val)
 	{
-		while (this->size() < n)
+		while (_size < n)
 		{
-			this->_array[this->size()] = val;
-			this->_size++;
+			_array[_size] = val;
+			_size++;
 		}
 	}
 
 	const 	std::string out_of_range_msg(size_type n) const
 	{
 		std::stringstream sstm;
-		sstm << "vector::_M_range_check: __n (which is " << n << ") this->size() (which is " << this->size() << ")" << std::endl;
+		sstm << "vector::_M_range_check: __n (which is " << n << ") this->size() (which is " << _size << ")" << std::endl;
 		std::string error_msg = sstm.str();
 		return error_msg;
 	}
@@ -77,47 +77,47 @@ public:
 
 	explicit vector (size_type n, const value_type& val = value_type(),const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _alloc(alloc)
 	{
-		this->_array = new value_type [n];
-		for (size_type i = 0; i < this->size(); i++)
-			this->_array[i] = val;
+		_array = new value_type [n];
+		for (size_type i = 0; i < _size; i++)
+			_array[i] = val;
 	}
 
 //	template <class InputIterator>
 //	vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _alloc(alloc) {
 //		for (iterator tmp = first; tmp != last; ++tmp)
 //		{
-//			this->_size++;
-//			this->_capacity++;
+//			_size++;
+//			_capacity++;
 //		}
-//		this->_array = new value_type [this->size()];
+//		_array = new value_type [_size];
 //		for (iterator tmp = first; tmp != last; ++tmp)
-//			this->_array = *tmp;
+//			_array = *tmp;
 //	}
 
 	vector& operator= (const vector& x) {
 		if (this != &x)
 		{
-			this->_size = x._size;
-			this->_capacity = x._capacity;
-			this->_alloc = x._alloc;
-			this->_array = new value_type [this->_size];
-			for (size_type i = 0; i < this->size(); i++)
-				this->_array[i] = x._array[i];
+			_size = x._size;
+			_capacity = x._capacity;
+			_alloc = x._alloc;
+			_array = new value_type [_size];
+			for (size_type i = 0; i < _size; i++)
+				_array[i] = x._array[i];
 		}
 		return *this;
 	}
 
 	// This one needs to be removed
 	vector (const vector& x){ *this = x; }
-	~vector() { delete [] this->_array; }
+	~vector() { delete [] _array; }
 
 //-> Iterators
 
-	iterator begin() { return iterator(&this->_array[0]); }
+	iterator begin() { return iterator(&_array[0]); }
 
 //	const_iterator begin() const;
 
-	iterator end() { return iterator(&this->_array[this->size()]); }
+	iterator end() { return iterator(&_array[_size]); }
 //	const_iterator end() const;
 
 	//** [iterator] rbegin (return a reverse iterator pointing to the last element in the vector)
@@ -130,25 +130,25 @@ public:
 
 //-> Capacity
 
-	size_type size() const { return this->_size; }
-	size_type max_size() const { return this->_alloc.max_size(); }
-	size_type capacity() const { return this->_capacity; }
+	size_type size() const { return _size; }
+	size_type max_size() const { return _alloc.max_size(); }
+	size_type capacity() const { return _capacity; }
 
 	void resize (size_type n, value_type val = value_type()) {
-		if (n < this->size())
-			this->_size = n;
-		else if (n > this->capacity() && n <= this->capacity() * 2) {
-			reallocation(this->capacity() * 2);
+		if (n < _size)
+			_size = n;
+		else if (n > _capacity && n <= _capacity * 2) {
+			reallocation(_capacity * 2);
 			copy_vector_value(n, val);
 		}
-		else if (n > this->capacity() * 2) {
+		else if (n > _capacity * 2) {
 			reallocation(n);
 			copy_vector_value(n, val);
 		}
 	}
 
 	bool empty() const {
-		if (!this->size())
+		if (!_size)
 			return true;
 		else
 			return false;
@@ -157,53 +157,53 @@ public:
 	void reserve (size_type n) {
 		if (n > this->max_size())
 			throw std::length_error("vector::reserve");
-		if (n > this->capacity())
+		if (n > _capacity)
 			reallocation(n);
 	}
 
 //-> Element access
 
 	reference operator[] (size_type n) {
-		reference elem_ref = *(this->_array + n);
+		reference elem_ref = *(_array + n);
 		return elem_ref;
 	}
 
 	const_reference operator[] (size_type n) const {
-		const_reference elem_ref = *(this->_array + n);
+		const_reference elem_ref = *(_array + n);
 		return elem_ref;
 	}
 
 	reference at (size_type n) {
-		if (n > this->_size - 1)
+		if (n > _size - 1)
 			throw std::out_of_range(out_of_range_msg(n));
-		reference elem_ref = *(this->_array + n);
+		reference elem_ref = *(_array + n);
 		return elem_ref;
 	}
 
 	const_reference at (size_type n) const {
-		if (n > this->_size - 1)
+		if (n > _size - 1)
 			throw std::out_of_range(out_of_range_msg(n));
-		const_reference elem_ref = *(this->_array + n);
+		const_reference elem_ref = *(_array + n);
 		return elem_ref;
 	}
 
 	reference front() {
-		reference front_ref = this->_array[0];
+		reference front_ref = _array[0];
 		return front_ref;
 	}
 
 	const_reference front() const {
-		const_reference front_ref = this->_array[0];
+		const_reference front_ref = _array[0];
 		return front_ref;
 	}
 
 	reference back() {
-		reference back_ref = this->_array[this->size() - 1];
+		reference back_ref = _array[_size - 1];
 		return back_ref;
 	}
 
 	const_reference back() const {
-		const_reference const_back_ref = this->_array[this->size() - 1];
+		const_reference const_back_ref = _array[_size - 1];
 		return const_back_ref;
 	}
 
@@ -213,27 +213,27 @@ public:
 //	void assign (InputIterator first, InputIterator last);
 
 	void assign (size_type n, const value_type& val) {
-		this->_size = n;
-		if (n <= this->capacity())
-			for (size_type i = 0; i < this->size(); i++)
-				this->_array[i] = val;
+		_size = n;
+		if (n <= _capacity)
+			for (size_type i = 0; i < _size; i++)
+				_array[i] = val;
 		else
 			reallocation_new_value(n, val);
 	}
 
 	void push_back (const value_type& val) {
-		if (this->size() + 1 > this->capacity())
+		if (_size + 1 > _capacity)
 		{
-			if (!this->size())
+			if (!_size)
 				reallocation(1);
 			else
-				reallocation(this->size() * 2);
+				reallocation(_size * 2);
 		}
-		this->_array[this->size()] = val;
-		this->_size += 1;
+		_array[_size] = val;
+		_size += 1;
 	}
 
-	void pop_back() { this->_size -= 1; }
+	void pop_back() { _size -= 1; }
 
 	iterator insert (iterator position, const value_type& val);
 
@@ -253,18 +253,18 @@ public:
 		size_type capacity_swap = x._capacity;
 		allocator_type alloc_swap = x._alloc;
 
-		x._array = this->_array;
-		x._size = this->_size;
-		x._capacity = this->_capacity;
-		x._alloc = this->_alloc;
+		x._array = _array;
+		x._size = _size;
+		x._capacity = _capacity;
+		x._alloc = _alloc;
 
-		this->_array = array_swap;
-		this->_size = size_swap;
-		this->_capacity = capacity_swap;
-		this->_alloc = alloc_swap;
+		_array = array_swap;
+		_size = size_swap;
+		_capacity = capacity_swap;
+		_alloc = alloc_swap;
 	}
 
-	void clear() { this->_size = 0; }
+	void clear() { _size = 0; }
 
 };
 }
