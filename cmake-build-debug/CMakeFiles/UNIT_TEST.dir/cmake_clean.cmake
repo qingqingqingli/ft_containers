7eadf3b3e4df8404ed1b9b812e06eeffae5165bf
qingqingqingli <kadlqbetty@gmail.com>
2021-08-05 15:26:42 +0200

    add all files

---
+++ cmake-build-debug/CMakeFiles/UNIT_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UNIT_TEST.dir/unit_test/catch2/catch.cpp.o"
+  "CMakeFiles/UNIT_TEST.dir/unit_test/random_access_iterator.cpp.o"
+  "CMakeFiles/UNIT_TEST.dir/unit_test/vector.cpp.o"
+  "UNIT_TEST"
+  "UNIT_TEST.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UNIT_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
